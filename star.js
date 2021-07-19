bb379efd07501c49052ace930bdb7919d71475fa
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-18 20:04:33 -0700

    add a nest loop and try alternative

--- star.js
+++ star.js
@@ -1,9 +1,14 @@
 function pattern(y) {
-    for (let i = 0; i < y; i++) {
-        for (let j = 0; j < y; j++) {
-            console.log("*");
-            
-        }
+    for (let i = 0; i <= y; i++) {
+        for (let j = 0; j <= y; j++) {
+            if (i>=j) {
+                console.log("*");
+            }else{
+                console.log(" ");
+            }
+            console.log("\n");
+           
+         }
         
         
     }

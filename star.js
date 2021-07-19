dcddae9f1ce4332100a1f3b444d0f5c81d968daf
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-19 07:18:41 -0700

    js file

---
+++ star.js
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title></title>
+</head>
+    <body>
+    <script type= "text/javascript">
+    let y = 6;
+    for (let i = 0; i < y; i++) {
+        for (let k=0; k<=(y-i); k++) {
+            console.log(" ");
+        }
+        for (let j = 0; j <= i; j++) {
+            console.log("*");
+            
+        }
+        console.log("\n");
+    }
+    </script>
+    </body>
+
+</html>

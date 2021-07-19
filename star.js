c472db411a8d67b3d838558c036b8ed02c422b75
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-18 23:31:10 -0700

    restructure the code

--- star.js
+++ star.js
@@ -1,11 +1,11 @@
 function pattern(y) {
     
-    for (let i = 1; i <=y; i++) {
-        for (let k = y-i; k > 0; k--) {
+    for (let i = 0; i < y; i++) {
+        for (let k=0; k<=(y-i); k++) {
             console.log(" ");
         }
-        for (let j = 1; j <= i; j++) {
-            console.log("* ");
+        for (let j = 0; j <= i; j++) {
+            console.log("*");
             
         }
         console.log("\n");

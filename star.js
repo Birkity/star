f35bd8587ebcd796efcca00e51effd48c6957ec1
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-19 07:27:48 -0700

    change back to js format

--- star.js
+++ star.js
@@ -1,21 +1,13 @@
-<html>
-<head>
-<title></title>
-</head>
-    <body>
-    <script type= "text/javascript">
-    let y = 6;
-    for (let i = 0; i < y; i++) {
-        for (let k=0; k<=(y-i); k++) {
-            console.log(" ");
-        }
-        for (let j = 0; j <= i; j++) {
-            console.log("*");
-            
-        }
-        console.log("\n");
-    }
-    </script>
-    </body>
 
-</html>
+
+ let y = 10;
+ for (let i = 0; i < y; i++) {
+     for (let k=0; k<=(y-i); k++) {
+         console.log(" ");
+     }
+     for (let j = 0; j <= i; j++) {
+         console.log("*");
+         
+     }
+     console.log("\n");
+ }

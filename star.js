baf1e72fc1a2d015663721c78d32c0645dc29e78
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-18 21:29:07 -0700

    Edits

--- star.js
+++ star.js
@@ -1,16 +1,14 @@
 function pattern(y) {
-    for (let i = 1; i <= y; i++) {
+    for (let i = 0; i < y; i++) {
         for (let k = y-i; k > 0; k--) {
             console.log(" ");
-            for (let j = 1; j <= i; j++) {
-               console.log("* ");
-              
-            }
-            console.log("\n");
         }
-        
-        
+            for (let j = 0; j <=i; j++) {
+               console.log("*");
+               }
+         console.log("\n");
         
     }
 }
-pattern(5);
+
+pattern(6);

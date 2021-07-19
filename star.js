cfa09aca430b21715bec26482277cee013799ea2
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-18 20:12:28 -0700

    add another nested loop and minor edits

--- star.js
+++ star.js
@@ -1,14 +1,13 @@
 function pattern(y) {
-    for (let i = 0; i <= y; i++) {
-        for (let j = 0; j <= y; j++) {
-            if (i>=j) {
-                console.log("*");
-            }else{
-                console.log(" ");
-            }
-            console.log("\n");
+    for (let i = 1; i <= y; i++) {
+        for (let k = y-i; k > 0; k--) {
+            console.log(" ");
+            for (let j = 1; j <= i; j++) {
+               console.log("* ");
            
-         }
+            }
+        }
+        
         
         
     }

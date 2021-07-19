474a4af5c60b954265fe20f0e11e244e21c831a0
Birkity Yishak <lilybluestorm7@gmail.com>
2021-07-19 07:28:53 -0700

    add function

--- star.js
+++ star.js
@@ -1,13 +1,15 @@
 
-
- let y = 10;
- for (let i = 0; i < y; i++) {
-     for (let k=0; k<=(y-i); k++) {
-         console.log(" ");
-     }
-     for (let j = 0; j <= i; j++) {
-         console.log("*");
-         
-     }
-     console.log("\n");
- }
+function star(y) {
+    for (let i = 0; i < y; i++) {
+        for (let k=0; k<=(y-i); k++) {
+            console.log(" ");
+        }
+        for (let j = 0; j <= i; j++) {
+            console.log("*");
+            
+        }
+        console.log("\n");
+    }
+    
+}
+ 
